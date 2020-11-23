4249f83a56b1422046bfa588fd99e529482e9763
xavier refour <xrthefrench@gmail.com>
2020-11-23 11:14:07 +0100

    2

--- appli2.py
+++ appli2.py
@@ -2,5 +2,18 @@
 # -*- coding: utf-8 -*-
 
 print("Hello, on calcule la tva")
-n  = int(input("entre le prix sans tva:"))
-print (n)
+n  = int(input("entre le prix hors taxes:"))
+quant  = int(input("entre une quantité:"))
+
+if n != "0":
+        print (" erreur entre une valeur superieur a 0")
+else:
+        if quant != "0":
+                print (" erreur entre une valeur superieur a 0")
+        else:
+            print ("prix sans tva" , (n)*(quant))
+tva = (n*quant)*1.2
+
+print ( "prix avec la tva" , (tva))
+tva > 200
+(tva) / 5
