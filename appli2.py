b13120fc2629c285b936904054381ee69436b2fd
xavier refour <xrthefrench@gmail.com>
2020-11-23 11:59:01 +0100

    exo 2

--- appli2.py
+++ appli2.py
@@ -5,15 +5,20 @@ print("Hello, on calcule la tva")
 n  = int(input("entre le prix hors taxes:"))
 quant  = int(input("entre une quantité:"))
 
-if n != "0":
-        print (" erreur entre une valeur superieur a 0")
-else:
-        if quant != "0":
-                print (" erreur entre une valeur superieur a 0")
-        else:
-            print ("prix sans tva" , (n)*(quant))
+#if n == "0":
+#        print (" erreur entre une valeur superieur a 0")
+#if quant == "0":
+#    print (" erreur entre une valeur superieur a 0")
+#    quit()
+# else:
+
+print ("prix sans tva" , (n)*(quant))
 tva = (n*quant)*1.2
 
 print ( "prix avec la tva" , (tva))
-tva > 200
-(tva) / 5
+
+if (tva) > 200:
+    rem= (tva)/5
+
+    print("remise de 5%", (rem))
+    print(" total avec la remise de 5%", (tva)-(rem))
