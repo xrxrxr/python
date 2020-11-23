434e9857d82fd245c100b34dc9df391467acf7f6
xavier refour <xrthefrench@gmail.com>
2020-11-23 17:23:47 +0100

    jolie table

--- appli3.py
+++ appli3.py
@@ -2,20 +2,27 @@
 # -*- coding: utf-8 -*-
 print("Hello, bienvenu dans notre boutique")
 
+from prettytable import PrettyTable
 
 
 
 
 produits = {
-    1:{"id": "1", "nom": "banane", "prix": "4", "quantité": "", "prixHT": ""},
-    2:{"id": "2", "nom": "pommme", "prix": "2", "quantité": "", "prixHT": ""},
-    3:{"id": "3", "nom": "orange", "prix": "1.5", "quantité": "", "prixHT": ""},
-    4:{"id": "4", "nom": "poire", "prix": "3", "quantité": "", "prixHT": ""},
-}
+            1: {'nom':'Banane', 'prix': 4, 'quantité':0},
+            2: {'nom':'Pomme', 'prix': 2,'quantité':0},
+            3: {'nom':'Orange', 'prix': 1.5, 'quantité':0},
+            4: {'nom':'Poire', 'prix': 3,'quantité':0}
+          }
 
+table = PrettyTable(["id",'nom','prix','quantité'])
 
+for (key,value) in produits.items():
+    table.add_row([key, value['nom'], value['prix'], value['quantité']])
 
-for item in produits:
+print (table)
 
-    print (produits(item, headers=["banane", "pommme", "orange", "poire"]))
-     #print(format('id', str,'nom','poire', str , "quantité" , "prixHT" , str),
+n1  = int(input("combien de bananes tu veux? "))
+n2  = int(input("combien de pommes tu veux? "))
+n3  = int(input("combien de oranges tu veux? "))
+n4  = int(input("combien de Poires tu veux? "))
+print("et de scoubidou bidou")
