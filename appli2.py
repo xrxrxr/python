69104b6180b635514feaa8c096f699ccd6c23b61
xavier refour <xrthefrench@gmail.com>
2020-11-23 10:22:15 +0100

    init

---
+++ appli2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+print("Hello, on calcule la tva")
+n  = int(input("entre le prix sans tva:"))
+print (n)
---
+++ appli3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+for i in range(5, 8) :
+    print(i, i ** 2)
+print('fin de boucle')
